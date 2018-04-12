39597fc3322810296ff0103a3fe208f09cc56f23
Efstathios Valanidis <evalanidis@gmail.com>
2018-04-12 22:12:03 +0300

    Add files via upload

--- FBullCowGame.h
+++ FBullCowGame.h
@@ -6,9 +6,11 @@ The game is a simple guess the word based on Mastermind
 #pragma once
 #include <string>
 #include <map>
+#include <vector>
 
 //to make syntax Unreal friendly
 #define TMap std::map 
+#define Vector std::vector 
 using FString = std::string;
 using int32 = int;
 
@@ -26,8 +28,7 @@ enum class EGuessStatus
 	Wrong_Length,
 	No_Lowercase,
 	Hint,
-	Error,
-	NoWordsRemain
+	Error
 };
 
 enum class EResetStatus {
@@ -47,10 +48,10 @@ public:
 	EGuessStatus CheckGuessValidity(FString) const;
 	void CreateDictionary();
 	int32 GetTotalHints() const;
-	void SetNumberOfWords(int32);
+	void SetTotalHints(int32);
 	int32 GetNumberOfWords() const;
 	void Reset();
-	char ShowLetterHint() const;
+	char ShowLetterHint();
 	FBullCowCount SubmitValidGuess(FString);
 	void Score();
 	void SetScore(int32);
@@ -60,15 +61,18 @@ public:
 	int32 GetRemainingWords() const;
 	bool CheckRemainingWords() const;
 
-
 private:
 	//see constructor for initialisation
 	int32 MyCurrentTry;
 	int32 ScoreResult;
 	FString MyHiddenWord;
 	int32 NumberOfWords;
+	int32 TotalHints;
+	int32 LengthOfDictionary;
 	int32 NumberOfRemainingWords;
-	TMap<int32, FString> Dictionary;
+	Vector<FString> Dictionary;
+	TMap<FString, bool> WordSeen;
+	TMap<char, bool> HintLetterCheckMap;
 	bool bGameIsWon;
 	bool IsIsogram(FString) const;
 	bool IsLowerCase(FString) const;
